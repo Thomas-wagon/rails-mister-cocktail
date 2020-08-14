f2e4c49d0901c5a6b35de58ed0855ad2efaabf65
thomas viaules <thomas.viaules@gmail.com>
2020-08-14 16:58:55 -0400

    still working on cocktail app

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'cocktails/index'
+  # get 'cocktails/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :index, :show] do
-    member do
-      get :ingredients do
-        member do
-          get :doses
-        end
-      end
-    end
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    # member do
+    #   get :ingredients do
+    #     member do
+    #       get :doses
+    #     end
+    #   end
+    # end
+    resources :doses, only: [ :new, :create ]
   end
+  resources :doses, only: [ :destroy ]
 end
